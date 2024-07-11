d11be800560c8e2b1c42dfea0d5dd4ca3d1c88a1
admin <shura__90@mail.ru>
2024-07-11 19:54:24 +0300

    'add index.html and some func'

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
+some_file_path = "index.html"
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def main():
+    return FileResponse(some_file_path)
