2760e7983e3aabe921f65520fccfb42821eeaf89
admin <shura__90@mail.ru>
2024-07-12 13:05:11 +0300

    'fixed some bugs'

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ app = FastAPI()
 
 
 @app.get("/")
-async def get_main_page():
-    return 'welcome to my main page'
+def get_main_page():
+    return 'welcome to my main page 11111111'
 
 
 @app.post("/user")
-async def create_user(user: User):
+def create_user(user: User):
     return {
         "name": user.name,
         "age": user.age,
