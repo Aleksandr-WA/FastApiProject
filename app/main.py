3aa53f3ee289f0d8a3f7a6512465fe3a3fd7fe6d
admin <shura__90@mail.ru>
2024-07-12 11:12:19 +0300

    'add folders and files for start app'

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+# новый роут
+@app.get("/custom")
+def read_custom_message():
+    return {"message": "This is a custom message!"}
+
+# uvicorn main:app --reload
